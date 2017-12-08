7b8ad0e6983acf6b8fb404b271ecbfc656d95877
Yuechaor <xxhiren@126.com>
2017-12-08 20:35:30 +1100

    forms

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import { Employee } from '../models/employee.model';
 
 @Component({
   selector: 'home',
@@ -6,4 +7,23 @@ import {Component} from '@angular/core';
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
+  languages: string[] = ['english','manderin','spanish','other'];
+  model = new Employee('','', true, 'w2', 'default');
+  hasLanguageError:boolean = false;
+
+  firstNameToUpperCase(value: string) {
+    if(value.length > 0)
+    this.model.firstName = value.charAt(0).toUpperCase() + value.slice(1);
+    else
+    this.model.firstName = value;
+  }
+  validateL(value) {
+    if (value === "default"){
+      // console.log("haha");      
+      this.hasLanguageError = true;
+    }else{
+      this.hasLanguageError = false;
+    }
+    
+  }
 }
